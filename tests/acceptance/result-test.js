eea4bb5c85a5eca505941054fa4f786e496a49fb
Justin Ball <justinball@gmail.com>
2014-06-06 11:05:10 -0600

    Messing with result test.

--- tests/acceptance/result-test.js
+++ tests/acceptance/result-test.js
@@ -1,19 +1,28 @@
-import startApp from '../helpers/start-app';
+// import startApp from '../helpers/start-app';
+// import theBikeResponse from '../fixtures/thebike.json';
 
-var App;
+// var ajax = require('ic-ajax');
 
-module('Integration - Result', {
-  setup: function() {
-    App = startApp();
-  },
-  teardown: function() {
-    Ember.run(App, 'destroy'); //comment this out to see the app in the state once the test it finished
-  }
-});
+// ajax.defineFixture('/fixtures/thebike.json', {
+//   response: theBikeResponse,
+//   jqXHR: {},
+//   textStatus: 'success'
+// });
 
-test('Visit result page', function() {
-  visit('/result/bike');
-  andThen(function() {
-    equal(find('#results').length, 1);
-  });
-});
+// var App;
+
+// module('Integration - Result', {
+//   setup: function() {
+//     App = startApp();
+//   },
+//   teardown: function() {
+//     Ember.run(App, 'destroy'); //comment this out to see the app in the state once the test it finished
+//   }
+// });
+
+// test('Visit result page', function() {
+//   visit('/result/bike');
+//   andThen(function() {
+//     equal(find('#results').length, 1);
+//   });
+// });
